cdb7d7aa4172d706350f6eca03044704ab2f0cba
hilmifawwazsaad <hilmi.saad31082004@gmail.com>
2024-03-21 01:45:15 +0700

    solve problem 00.00

--- task-4/isabel-a.sh
+++ task-4/isabel-a.sh
@@ -1,41 +1,83 @@
 #!/bin/bash
 
-simpan_foto() {
-    #waktu saat ini dalam format jam dan menit
-    waktu_sekarang=$(date +%H.%M)
+# Function to download photos
+download_photos() {
+    local count=$1
+    local folder=$2
 
-    #variabel diikuti argumen
-    nama_folder="folder_$1"
+    mkdir -p "$folder"
+    for ((i=1; i<=$count; i++)); do
+        # Use direct image URLs for downloading photos
+        wget --show-progress --progress=bar -q -O "$folder/foto_$i.jpg" "https://phinemo.com/wp-content/uploads/2017/02/hidup-bebas.jpg"
+        echo "Downloaded foto_$i.jpg to $folder"
+    done
+}
 
-    #membuat direktori baru
-    mkdir -p "$nama_folder"
+# Function to check if current time is even
+is_even() {
+    local time=$1
+    ((time % 2 == 0))
+}
 
-    #kondisi ketika waktu 00.00
-    if [[ $waktu_sekarang == "00.00" ]];
-    then
-        for ((i=1; i<=10; i++));
-        do
-            wget -O "$nama_folder/foto_$i.jpg" "https://drive.google.com/drive/folders/11o9WxJ_ogiWBC4jSyfWxw9mU5726hVCw"
-        done
-    #kondisi ketika waktu genap
-    elif (( $(echo "$waktu_sekarang" | cut -d. -f1) % 2 == 0 ));
-    then
-        for ((i=1; i<=5; i++));
-        do
-            wget -O "$nama_folder/foto_$i.jpg" "https://drive.google.com/drive/folders/11o9WxJ_ogiWBC4jSyfWxw9mU5726hVCw"
-        done
-    #kondisi ketika waktu ganjil
+# Function to check if current time is 00:00
+is_midnight() {
+    local hour=$1
+    local minute=$2
+    [[ $hour -eq 0 && $minute -eq 0 ]]
+}
+
+# Variable to store the last execution time
+#last_execution_time=0
+
+# Function to get the last execution time
+#get_last_execution_time() {
+#    echo "$last_execution_time"
+#}
+
+# Function to update the last execution time
+update_last_execution_time() {
+    last_execution_time=$(date +%s)
+    echo "Last execution time updated: $(date)"
+}
+
+# Main function
+main() {
+    local current_hour=$(date +"%H")
+    local current_minute=$(date +"%M")
+    local last_folder_number=$(ls -d folder_* 2>/dev/null | wc -l)
+    local folder_counter=$((last_folder_number + 1))
+
+    echo "Current hour: $current_hour, Current minute: $current_minute"
+
+    # Get the last execution time
+    #local last_execution=$(get_last_execution_time)
+    #echo "Last execution time: $(date -d @$last_execution)"
+
+    # Calculate the time difference in hours
+    local time_diff=$(( ( $(date +%s) - last_execution ) / 3600 ))
+
+    # If it's midnight, download 10 photos
+    if is_midnight "$current_hour" "$current_minute"; then
+        folder_counter=$((folder_counter + 1))
+        download_photos 10 "folder_$folder_counter"
+        echo "Downloaded 10 photos at midnight."
     else
-        for ((i=1; i<=3; i++));
-        do
-            wget -O "$nama_folder/foto_$i.jpg" "https://drive.google.com/drive/folders/11o9WxJ_ogiWBC4jSyfWxw9mU5726hVCw"
-        done
+        # If the time difference is greater than or equal to 5 hours, proceed with the execution
+        if (( time_diff >= 5 )); then
+            if is_even "$current_hour"; then
+                download_photos 5 "folder_$folder_counter"
+                echo "Downloaded 5 photos at even hour."
+            else
+                download_photos 3 "folder_$folder_counter"
+                echo "Downloaded 3 photos at odd hour."
+            fi
+
+            # Update the last execution time
+            update_last_execution_time
+        else
+            echo "No action taken. Exiting."
+        fi
     fi
 }
 
-#jam saat ini
-jam_sekarang=$(date +%H)
-#menghitung nomor folder berdasarkan jam
-angka_folder=$((jam_sekarang / 5 + 1))
-
-simpan_foto "$angka_folder"
+main
