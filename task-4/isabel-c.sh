c4058b04071b8eff0a9262158342a85becfd84c1
hilmifawwazsaad <hilmi.saad31082004@gmail.com>
2024-03-21 03:20:18 +0700

    solve error looping

--- task-4/isabel-c.sh
+++ task-4/isabel-c.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 #letak directory peyimpanan folder dan zip
-directory="/home/hilmifawwaz/sisop/praktikum-modul-1-d04/task4"
+directory="/home/hilmifawwaz/sisop/praktikum-modul-1-d04/task-4"
 
-#variabel boolean untuk inisialisasi folder
+#variabel boolean untuk inisialisasi folder yang dicari
 folder_ditemukan=false
 #cek apakah ada folder dalam directory, jika ada hapus
 for folder in "$directory"/folder_*; do
-    if [ -d "$folder" ];
-    then
+    if [ -d "$folder" ]; then
         folder_ditemukan=true
         rm -rf "$folder" #hapus folder
         echo "Folder $folder telah dihapus"
@@ -16,25 +15,22 @@ for folder in "$directory"/folder_*; do
 done
 
 #kondisi jika tidak ada folder di directory
-if ! $folder_ditemukan;
-then
+if ! $folder_ditemukan; then
     echo "Tidak ada folder di direktori $directory"
 fi
 
-#variabel boolean untuk inisialisasi zip
+#variabel boolean untuk inisialisasi zip yang dicari
 zip_ditemukan=false
 #cek apakah ada file zip di direktori, jika ada hapus
 for zip_file in "$directory"/ayang_*.zip; do
-    if [ -f "$zip_file" ]; 
-    then
-        folder_ditemukan=true
+    if [ -f "$zip_file" ]; then
+        zip_ditemukan=true
         rm -f "$zip_file" #hapus file zip
         echo "File zip $zip_file telah dihapus"
     fi
 done
 
 #kondisi jika tidak ada file zip dalam directory
-if ! $zip_ditemukan;
-then
+if ! $zip_ditemukan; then
     echo "Tidak ada zip di direktori $directory"
 fi
