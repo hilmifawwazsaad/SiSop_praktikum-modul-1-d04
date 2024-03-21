9e27eabdd01c51fc530cc671d966d0e2c6c9ceb1
hilmifawwazsaad <hilmi.saad31082004@gmail.com>
2024-03-21 21:20:42 +0700

    fix: metrics don't appear

--- task-3/minute_log.sh
+++ task-3/minute_log.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-# Set target path
+#target path yang akan dimonitoring
 target_path="/home/$(whoami)/"
 
-# Save path
+#path penyimpanan log
 save_path="/home/$(whoami)/metrics"
 
-# Create path for save the log
-mkdir -p "$save_path" || { echo "Failed to create directory $save_path"; exit 1; }
+#buat direktori untuk menyimpan log
+mkdir -p "$save_path" || { echo "Gagal membuat direktori $save_path"; exit 1; }
 
-# Set log file name pattern
-log_file_pattern="$save_path/metrics_$(date +"%Y%m%d%H%M%S").log"
+#pola nama file log
+log_file="$save_path/metrics_$(date +"%Y%m%d%H%M%S").log"
 
-# Function to log metrics
+#fungsi untuk mencatat metrik
 log_metrics() {
-  # RAM usage metrics
-  echo "mem_total,mem_used,mem_free,mem_shared,mem_buff,mem_available,swap_total,swap_used,swap_free,path,path_size" >> "$log_file_pattern"
-  free -m | awk -v target_path="$target_path" 'NR==2 {printf "%d,%d,%d,%d,%d,%d,%d,%d,%d,%s,%s\n", $2, $3, $4, $5, $6, $7, $8, $9, $10, target_path, $NF }' OFS=, >> "$log_file_pattern"
+  #metrik penggunaan RAM
+  ram_metrics=$(free -m | awk 'NR==2 {printf "%d,%d,%d,%d,%d,%d", $2, $3, $4, $5, $6, $7}')
 
-  # Disk usage metrics
-  echo "Disk usage for path $target_path at $(date):" >> "$log_file_pattern"
-  du -sh "$target_path" | awk -v target_path="$target_path" '{printf "%s,%s\n", target_path, $1 }' OFS=, >> "$log_file_pattern"
+  #metrik memori swap
+  swap_metrics=$(free -m | awk 'NR==3 {printf ",%d,%d,%d", $2, $3, $4}')
+
+  #metrik penggunaan disk
+  disk_metrics=$(du -sh "$target_path" | awk -v target_path="$target_path" '{printf ",%s,%s\n", target_path, $1 }')
+
+  #menulis metrik ke file log
+  echo "mem_total,mem_used,mem_free,mem_shared,mem_buff,mem_available,swap_total,swap_used,swap_free,path,path_size" >> "$log_file"
+  echo "$ram_metrics$swap_metrics$disk_metrics" >> "$log_file"
 }
 
-# Log metrics
+#cetak metrik
 log_metrics
